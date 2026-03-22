fbf9a68f78416a3376c562404778ca06d864fee6
iansm <ians.mcvay@gmail.com>
2026-03-22 15:17:58 -0700

    added javascript updates, including exercise array and displayExercises function

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
+const exerciseList = document.getElementById("exerciseList");
+
 const exercises = [
     {
         bodyPart: "neck",
-        name: "chin tuck",
+        name: "Chin Tuck",
         description: "Roll your chin down to your chest as if looking at your upper chest.",
         parameters: "3 sets, 10 reps",
         image: "url",
     },
     {
         bodyPart: "shoulder",
-        name: "scap retraction",
+        name: "Scap Retraction",
         description: "Pinch your shoulder blades together.",
         parameters: "3 sets, 10 reps",
         image: "url",
     },
     {
         bodyPart: "hip",
-        name: "bridge",
+        name: "Bridge",
         description: "Lift your buttocks in the air.",
         parameters: "3 sets, 10 reps",
         image: "url",
     }
 ]
+
+function displayExercises(list) {
+    exerciseList.innerHTML = "";
+
+    list.forEach(exercise => {
+        exerciseList.innerHTML += `
+            <div class="card">
+                <img src="${exercise.image}" alt="${exercise.name}">
+                <h3>${exercise.name}</h3>
+                <p>${exercise.description}</p>
+                <p><strong>${exercise.parameters}</strong></p>
+        `;
+    });
+}
+
+displayExercises(exercises);
