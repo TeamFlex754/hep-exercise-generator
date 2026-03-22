4aba25be5993565551f1c4f5c4b72c412c2fcfc5
iansm <ians.mcvay@gmail.com>
2026-03-22 14:35:06 -0700

    created exercise array of objects

---
+++ script.js
@@ -0,0 +1,23 @@
+const exercises = [
+    {
+        bodyPart: "neck",
+        name: "chin tuck",
+        description: "Roll your chin down to your chest as if looking at your upper chest.",
+        parameters: "3 sets, 10 reps",
+        image: "url",
+    },
+    {
+        bodyPart: "shoulder",
+        name: "scap retraction",
+        description: "Pinch your shoulder blades together.",
+        parameters: "3 sets, 10 reps",
+        image: "url",
+    },
+    {
+        bodyPart: "hip",
+        name: "bridge",
+        description: "Lift your buttocks in the air.",
+        parameters: "3 sets, 10 reps",
+        image: "url",
+    }
+]
